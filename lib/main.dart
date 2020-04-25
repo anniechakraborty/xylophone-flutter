f81f3eccff3e6e4c07f58d9264400dc2a77dd735
Angela <8798027+angelabauer@users.noreply.github.com>
2020-04-25 21:53:58 +0530

    The app has seven distinct notes in seven differently coloured flat buttons. Pressing the buttons generates different sounds.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber){
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+  Expanded buildKey({int noteNumber, Color colour}){
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.all(0.0),
+        onPressed: (){
+          playSound(noteNumber);
+        },
+        color: colour,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(colour : Colors.redAccent, noteNumber: 1),
+              buildKey(colour : Colors.deepOrangeAccent, noteNumber: 2),
+              buildKey(colour : Colors.orangeAccent, noteNumber: 3),
+              buildKey(colour : Colors.yellow.shade400, noteNumber: 4),
+              buildKey(colour : Colors.tealAccent, noteNumber: 5),
+              buildKey(colour : Colors.lightBlueAccent, noteNumber: 6),
+              buildKey(colour : Colors.blue.shade700, noteNumber: 7),
+              ],
+          ),
         ),
       ),
     );
